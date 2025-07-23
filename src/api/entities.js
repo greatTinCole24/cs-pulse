89a38035f95fd0240a0d20f96428d3475d253f0f
greatTinCole24 <54828133+greatTinCole24@users.noreply.github.com>
2025-07-23 12:00:28 -0400

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,17 @@
+import { base44 } from './base44Client';
+
+
+export const Player = base44.entities.Player;
+
+export const Team = base44.entities.Team;
+
+export const VideoAnalysis = base44.entities.VideoAnalysis;
+
+export const Match = base44.entities.Match;
+
+export const ScoutingReport = base44.entities.ScoutingReport;
+
+
+
+// auth sdk:
+export const User = base44.auth;
